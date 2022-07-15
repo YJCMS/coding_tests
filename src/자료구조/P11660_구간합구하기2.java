335961a5d8f278ff72e5b0926f1282767ca09698
YJCMS <sen9563@gmail.com>
2022-07-16 05:57:04 +0900

    자료구조

--- src/자료구조/P11660_구간합구하기2.java
+++ src/자료구조/P11660_구간합구하기2.java
@@ -7,12 +7,14 @@ import java.util.StringTokenizer;
 
 public class P11660_구간합구하기2 {
     public static void main(String[] args) throws IOException {
+        // N 배열크기 M 질의 수 저장하기
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
         int A[][] = new int[N+1][N+1];
 
+        // 원본 배열 저장
         for (int i=1; i<=N; i++) {
             st = new StringTokenizer(br.readLine());
             for (int j=1; j<=N; j++) {
@@ -20,6 +22,7 @@ public class P11660_구간합구하기2 {
             }
         }
 
+        // 합 배열 저장 (D[i][j] = D[i][j-1] + D[i-1][j] - D[i-1][j-1] + A[i][j])
         int D[][] = new int[N+1][N+1];
         for (int i=1; i<=N; i++) {
             for (int j=1; j<=N; j++) {
@@ -27,6 +30,8 @@ public class P11660_구간합구하기2 {
             }
         }
 
+        // 질의 계산 및 출력
+        // 질의 x1, y1, x2, y2에 대한 답을 구간합으로 구하는 방법 (D[x2][y2] - D[x1-1][y2] - D[x2][y1-1] + D[x1-1][y1-1])
         for (int i=0; i<M; i++) {
             st = new StringTokenizer(br.readLine());
             int x1 = Integer.parseInt(st.nextToken());
